79718322830a9e5e491edbf1907dc5053eb38a2d
mcontrerassc <mcontrerascatalan25@amherst.edu>
2025-09-28 19:21:57 -0300

    Eliminar lista restringida en H1

--- unified/heuristica1.py
+++ unified/heuristica1.py
@@ -17,7 +17,6 @@ def calcular_puntos_de_pickup(workers, distancias):
     return puntos_pickup
 
 def heur_grasp(alpha, N, distancias, puntos_pickup, trabajadores):
-    k = 10
     ruta = []
     trabajadores_sin_recoger = set(range(len(trabajadores)))
     nodo_actual = 0
@@ -56,9 +55,6 @@ def heur_grasp(alpha, N, distancias, puntos_pickup, trabajadores):
         for c in opciones:
             if c[1] >= lower_bound:
                 rcl.append(c)
-                # limitar tamaño máximo de la RCL
-        if k is not None and len(rcl) > k:
-            rcl = rcl[:k]
 
         nodo_escogido, _, trabajadores_recogidos = random.choice(rcl)
         ruta.append(nodo_escogido)
